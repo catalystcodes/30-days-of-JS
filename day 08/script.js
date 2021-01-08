7549dc1ed38a7153a33d9edaa7294847588fb1a3
mubby101 <muh4mub@gmail.com>
2021-01-08 09:55:34 +0100

    feat: day 08-Execrise_Level-1

--- day 08/script.js
+++ day 08/script.js
@@ -49,3 +49,74 @@ const dog = {
 
 console.log(dog.getDogInfo())
 console.log(dog)
+
+
+/*
+    Execrises: Level 2
+*/
+
+const users = {
+    Alex: {
+        email: 'alex@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript'],
+        age: 20,
+        isLoggedIn: false,
+        points: 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+    },
+    Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+    },
+    Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+    },
+    Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    },
+    Paul: {
+        email: 'paul@paul.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+    }
+}
+const people = Object.keys(users)
+
+const skillsLength = people.map(person => {
+    const personData = users[person]
+    return personData.skills.length
+})
+
+const highestSkills = Math.max(...skillsLength)
+
+const highestPerson = people.filter(person => {
+    return users[person].skills.length == highestSkills
+})
+
+console.log(highestPerson.join(', '), highestSkills)
