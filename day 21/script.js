dff6651c986c2d1f7f2636242e2caaceeb35f316
muhammad <muh4mub@gmail.com>
2021-06-15 10:05:50 +0100

    day21: Execise1

---
+++ day 21/script.js
@@ -0,0 +1,19 @@
+//level1
+
+const paragraph = document.querySelector("p")
+paragraph.style.color = "blue"
+
+const useId = document.querySelector('#id1')
+useId.style.fontSize = "1.5rem"
+
+const useId2 = document.querySelector('#id2')
+useId2.style.fontStyle = "italic"
+
+const useId3 = document.querySelector('#id3')
+useId3.style.fontWeight = 'bold'
+
+const nodeList = document.querySelectorAll('p')
+
+for (let i = 0; i < nodeList.length; i++) {
+  console.log(nodeList[i])
+}
