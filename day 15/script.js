ec2af670c9fc6867fee8dc85223e53cd05622efb
muhammad <muh4mub@gmail.com>
2021-12-29 14:38:00 +0100

    feat: day 15 completed

--- day 15/script.js
+++ day 15/script.js
@@ -192,19 +192,31 @@ console.log(s1)
 //Exercises Level 1
 
 class Animal {
-	constructor (name, age, color, legs) {
-		this.name = name
-		this.age = age
-		this.color = color
-		this.legs = legs
-	}
-}
-
-const ant = new Animal('Ant', 1, 'Black', 6)
-const spider = new Animal('Spider', 3, 'Black', 8)
-const cat = new Animal('Cat', 4, 'white', 4)
-const dog = new Animal('Cat', 4, 'Brown', 4)
-
-console.log(cat)
-console.log(ant)
-console.log(spider)
+    constructor (name,age,color,legs){
+        this.name = name
+        this.age = age
+        this.color = color
+        this.legs = legs
+    }
+    makesouund(){
+        return (`${this.name} is chasing rats !!`)
+    }
+  }
+  const  animal = new Animal ('jack', '2', 'brown', '4')
+  console.log(animal);
+  console.log(animal.makesouund());
+  class Cats extends Animal {}
+  class Dogs extends Animal {
+      constructor (name,age,color,legs,sound){
+          super (name,age,color,legs)
+         this.sound = sound
+      }
+      dog () {
+          return (`${this.name} has ${this.legs} legs and a ${this.color} body and of ${this.age} years old`)
+      }
+  }
+  const woll = new Dogs ('Tiger', 4, 'black', 4, 'gbo, gbo')
+  const dark = new Cats ('kitty', 5, 'white', 4, 'meow, meow')
+  console.log(woll);
+  console.log(dark);
+  console.log(woll.dog());
