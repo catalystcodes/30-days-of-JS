811b05ef2cc99acaebc435f8c01507d149a0417e
muhammad <muh4mub@gmail.com>
2022-04-07 14:44:50 +0100

    eventlistener-update database

--- day 23/script.js
+++ day 23/script.js
@@ -5,6 +5,5 @@ window.addEventListener("keydown", (e) => {
 	document.querySelector("#key").textContent = e.key;
 	document.querySelector("#code").textContent = e.keyCode;
 	document.querySelector("#code").style.padding = "2rem 2rem";
-	document.querySelector("#code").style.boxShadow =
-	  "0px 0px 5px 1px lightgrey";
-  });
+	document.querySelector("#code").style.boxShadow = "0px 0px 5px 1px lightgrey";
+});
